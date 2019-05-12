3136242482956e2d4f46b5cff6cf2219e807364b
Oskar <oskar@cindori.org>
2019-05-12 17:08:20 +0200

    Init

---
+++ MetalBezier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalBezier
+//
+//  Created by Oskar Groth on 2019-05-12.
+//  Copyright © 2019 Oskar Groth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
